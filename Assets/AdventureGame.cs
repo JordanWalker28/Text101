24c6f96ae4b045f95f97819cd75220a8e2ce7c52
Jordan Walker <jordanswalker28@gmail.com>
2020-06-27 00:29:42 +0100

    Added states to the game

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "Welcome to the game";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
