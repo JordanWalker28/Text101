3d95632c2823f39daea54e8758ae3faf372734a3
Jordan Walker <jordanswalker28@gmail.com>
2020-06-29 00:50:14 +0100

    Added controls to the game

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+
+    State state;
+
+    void Start()
+    {
+        state = StartingState;
+        getStateStory();
+    }
+
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            getStateStory();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+            getStateStory();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+            getStateStory();
+        }
+        else if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void getStateStory()
+    {
+        textComponent.text = state.GetStateStory();
+    }
+}
